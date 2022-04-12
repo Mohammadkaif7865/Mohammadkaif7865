54b12b0aa9bb276fb628c82b8c651b1a7d599141
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-04-12 08:35:54 +0530

    yes me

--- myscript.js
+++ myscript.js
@@ -1,14 +1,14 @@
-let more_likes = 0;
+let more_likes = 1;
 let already_likes = 203;
 
 function like() {
-    more_likes++;
     if (more_likes % 2 == 0) {
         document.getElementById('like').style.color = "black";
         already_likes--;
     } else {
-        document.getElementById('like').style.color = "red";
+        document.getElementById('like').style.cssText = "animation-name: heart-pop; animation-duration: 1s; animation-timing-function: ease-out; animation-iteration-count: 1; color:red;"
         already_likes++;
     }
     document.getElementById('counts').innerHTML = `${already_likes} likes`;
+    more_likes++;
 }
